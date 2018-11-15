4231bd085386c311b659de1aa936f42525f0a18f
devil1165 <devkant@getambee.com>
2018-11-15 18:52:36 +0530

    something

---
+++ api/models/readinModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const readingSchema = mongoose.Schema({
+    pm25: {
+        type:Number
+    },
+    pm1: {
+        type:Number
+    },
+    pm10: {
+        type:Number
+    },
+    battery: {
+        type:Number
+    },
+    
+    location: {
+        type:{
+            type: String, // Don't do `{ location: { type: String } }`
+            enum: ['Point'], // 'location.type' must be 'Point'
+        },
+        coordinates: {
+            type: [Number],
+        }
+    },
+    timestamp:{
+        type:Date,
+        required:true
+    },
+    user:{ 
+        type:String, 
+        // require:true 
+    }
+})
+
+module.exports = mongoose.model('Reading',readingSchema);
+   
